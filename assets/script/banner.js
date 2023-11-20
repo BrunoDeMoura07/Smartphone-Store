a54aa8e82c1a04f78058bff856992aa719ae8a5f
Bruno de Moura <brunnofarias16@gmail.com>
2023-11-20 17:41:37 -0300

    testes de slide

--- assets/script/banner.js
+++ assets/script/banner.js
@@ -1,23 +1,20 @@
 const bannerImage = document.getElementById("banner-image");
 let currentIndex = 0;
 
-
-bannerImage.addEventListener('load', function() {
-    setTimeout(() => {
-        bannerImage.classList.remove('fade-in-animation');
-    }, 1000);
+bannerImage.addEventListener("load", function () {
+  setTimeout(() => {
+    bannerImage.classList.remove("fade-in-animation");
+  }, 1000);
 });
 
 setInterval(changeImage, 11000);
 
-const images = ['./assets/img/banner2.jpg', './assets/img/img1.jpg'];
+const images = ["./assets/img/banner2.jpg", "./assets/img/img1.jpg"];
 
 function changeImage() {
-    currentIndex = (currentIndex + 1) % images.length;
+  currentIndex = (currentIndex + 1) % images.length;
 
-    bannerImage.classList.add('fade-in-animation');
+  bannerImage.classList.add("fade-in-animation");
 
-    bannerImage.src = images[currentIndex];
+  bannerImage.src = images[currentIndex];
 }
-
-
